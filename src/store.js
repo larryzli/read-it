5274a960b1b528d24df17df9bac585fbc866ab31
Jordan Snow <snowstorm1989@gmail.com>
2018-02-12 17:17:22 -0600

    Fixed Port Number

--- src/store.js
+++ src/store.js
@@ -2,19 +2,11 @@ import { createStore, applyMiddleware, combineReducers } from "redux";
 import promiseMiddleware from "redux-promise-middleware";
 
 //Importing Reducers
-<<<<<<< HEAD
-import userReducer from './ducks/userReducer'
-import subredditReducer from './ducks/subredditReducer'
-import postRedeucer from './ducks/postReducer'
-import commentReducer from './ducks/commentReducer'
-import messageReducer from './ducks/messageReducer'
-=======
 import userReducer from "./ducks/userReducer";
 import subredditReducer from "./ducks/subredditReducer";
 import postReducer from "./ducks/postReducer";
 import commentReducer from "./ducks/commentReducer";
 import messageReducer from "./ducks/messageReducer";
->>>>>>> c02c7a24120b3661315b7d713f11634134faa986
 
 //Combining Reducers
 const reducers = combineReducers({
