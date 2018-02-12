45c75447bdf9755798df29815b691e4491254dbd
Jordan Snow <snowstorm1989@gmail.com>
2018-02-12 16:44:53 -0600

    Redux Store Made

---
+++ src/store.js
@@ -0,0 +1,22 @@
+import { creatStore, applyMiddleware, combineReducers, applyMiddleware } from 'redux'
+import promiseMiddleware from 'redux-promise-middleware'
+
+
+//Importing Reducers
+import userReducer from './ducks/userReducer'
+import subreddditReducer from './ducks/subredditReducer'
+import postRedeucer from './ducks/postReducer'
+import commentReducer from './ducks/commentReducer'
+import messageReducer from './ducks/messageReducer'
+
+//Combining Reducers
+const reducers = combineReducers({
+    user: userReducer,
+    subreddit: subredditReducer,
+    post: postReducer,
+    comment: commentReducer,
+    message: messageReducer
+})
+const store = createStore(reducers, applyMiddleware(promiseMiddleware()));
+
+export default store;
