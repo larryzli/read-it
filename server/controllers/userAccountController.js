a59156299eca9208768eb73152137ea7a4c279de
Larry Li <larry.zy.li@gmail.com>
2018-02-13 16:19:54 -0600

    added more styling, url and permalink to data

--- server/controllers/userAccountController.js
+++ server/controllers/userAccountController.js
@@ -1,103 +1,105 @@
 const axios = require("axios");
 
 const getUserById = (req, res, next) => {
-  const db = req.app.get("db");
-  const { user_id } = req.params;
-  db
-    .getUserById(user_id)
-    .then(response => {
-      res.status(200).json(response[0]);
-    })
-    .catch(console.log);
+    const db = req.app.get("db");
+    const { user_id } = req.params;
+    db
+        .getUserById(user_id)
+        .then(response => {
+            res.status(200).json(response[0]);
+        })
+        .catch(console.log);
 };
 
 const createUserById = (req, res, next) => {
-  const db = req.app.get("db");
-  const { user_id } = req.params;
-  db
-    .createUserById(user_id)
-    .then(response => {
-      res.status(200).json(response[0]);
-    })
-    .catch(console.log);
+    const db = req.app.get("db");
+    const { user_id } = req.params;
+    db
+        .createUserById(user_id)
+        .then(response => {
+            res.status(200).json(response[0]);
+        })
+        .catch(console.log);
 };
 
 const pullHot = (req, res, next) => {
-  let baseURL = "https://www.reddit.com/hot.json?";
-  const { limit, after } = req.query;
-  if (req.query) {
-    if (limit) {
-      baseURL += `limit=${limit}&`;
+    let baseURL = "https://www.reddit.com/hot.json?";
+    const { limit, after } = req.query;
+    if (req.query) {
+        if (limit) {
+            baseURL += `limit=${limit}&`;
+        }
+        if (after) {
+            baseURL += `after=${after}&`;
+        }
     }
-    if (after) {
-      baseURL += `after=${after}&`;
-    }
-  }
-  axios
-    .get(baseURL)
-    .then(response =>
-      res.status(200).json({
-        posts: response.data.data.children.map(el => {
-          let o = el.data;
-          return {
-            domain: o.domain,
-            subreddit_title: o.subreddit,
-            subreddit_id: o.subreddit_id,
-            author: o.author,
-            post_title: o.title,
-            num_comments: o.num_comments,
-            score: o.score,
-            created_utc: o.created_utc,
-            post_id: o.id,
-            post_thumbnail: o.thumbnail,
-            over_18: o.over_18
-          };
-        }),
-        after: response.data.data.after
-      })
-    )
-    .catch(console.log);
+    axios
+        .get(baseURL)
+        .then(response =>
+            res.status(200).json({
+                posts: response.data.data.children.map(el => {
+                    let o = el.data;
+                    return {
+                        domain: o.domain,
+                        subreddit_title: o.subreddit,
+                        subreddit_id: o.subreddit_id,
+                        author: o.author,
+                        post_title: o.title,
+                        num_comments: o.num_comments,
+                        score: o.score,
+                        created_utc: o.created_utc,
+                        post_id: o.id,
+                        post_thumbnail: o.thumbnail,
+                        over_18: o.over_18,
+                        url: o.url,
+                        permalink: o.permalink
+                    };
+                }),
+                after: response.data.data.after
+            })
+        )
+        .catch(console.log);
 };
 
 const pullBest = (req, res, next) => {
-  let baseURL = "https://www.reddit.com/best.json?";
-  const { limit, after } = req.query;
-  if (req.query) {
-    if (limit) {
-      baseURL += `limit=${limit}&`;
-    } else if (after) {
-      baseURL += `after=${after}&`;
+    let baseURL = "https://www.reddit.com/best.json?";
+    const { limit, after } = req.query;
+    if (req.query) {
+        if (limit) {
+            baseURL += `limit=${limit}&`;
+        } else if (after) {
+            baseURL += `after=${after}&`;
+        }
     }
-  }
-  axios
-    .get(baseURL)
-    .then(response =>
-      res.status(200).json({
-        posts: response.data.data.children.map(el => {
-          let o = el.data;
-          return {
-            domain: o.domain,
-            subreddit_title: o.subreddit,
-            subreddit_id: o.subreddit_id,
-            author: o.author,
-            post_title: o.title,
-            num_comments: o.num_comments,
-            score: o.score,
-            created_utc: o.created_utc,
-            post_id: o.id,
-            post_thumbnail: o.thumbnail,
-            over_18: o.over_18
-          };
-        }),
-        after: response.data.data.after
-      })
-    )
-    .catch(console.log);
+    axios
+        .get(baseURL)
+        .then(response =>
+            res.status(200).json({
+                posts: response.data.data.children.map(el => {
+                    let o = el.data;
+                    return {
+                        domain: o.domain,
+                        subreddit_title: o.subreddit,
+                        subreddit_id: o.subreddit_id,
+                        author: o.author,
+                        post_title: o.title,
+                        num_comments: o.num_comments,
+                        score: o.score,
+                        created_utc: o.created_utc,
+                        post_id: o.id,
+                        post_thumbnail: o.thumbnail,
+                        over_18: o.over_18
+                    };
+                }),
+                after: response.data.data.after
+            })
+        )
+        .catch(console.log);
 };
 
 module.exports = {
-  getUserById,
-  createUserById,
-  pullHot,
-  pullBest
+    getUserById,
+    createUserById,
+    pullHot,
+    pullBest
 };
