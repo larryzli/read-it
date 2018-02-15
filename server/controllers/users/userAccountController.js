6aa119f9e29cad31d2c7968e0eb442a30e8d04ca
Bruno Carvalho <dipsnatchy26@gmail.com>
2018-02-15 12:35:58 -0600

    fix authorization problem

--- server/controllers/users/userAccountController.js
+++ server/controllers/users/userAccountController.js
@@ -8,6 +8,27 @@ const getUserInfo = (req, res, next) => {
   }
 };
 
+const getAllFriends = (req, res, next) => {
+  axios
+    .get("https://oauth.reddit.com/api/v1/me/friends", {
+      headers: {
+        Authorization: `bearer ${req.user.accessToken}`
+      }
+    })
+    .then(response => res.status(200).json(response.data.data.children))
+    .catch(console.log);
+};
+
+const reply = (req, res, next) => {
+  const { id, text } = req.body;
+  axios
+    .post(`https://www.reddit.com/api/comment?thing_id=${id}&text=${text}`)
+    .then(response => console.log(response))
+    .catch(console.log);
+};
+
 module.exports = {
-  getUserInfo
+  getUserInfo,
+  getAllFriends,
+  reply
 };
--- server/controllers/users/usersRoute.js
+++ server/controllers/users/usersRoute.js
@@ -1,5 +1,11 @@
-const { getUserInfo } = require("./userAccountController");
+const {
+  getUserInfo,
+  getAllFriends,
+  reply
+} = require("./userAccountController");
 
 module.exports = function(app) {
   app.get("/api/user/info", getUserInfo);
+  app.get("/api/friends/get", getAllFriends);
+  app.post("/api/reply", reply);
 };
