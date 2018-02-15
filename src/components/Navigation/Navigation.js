c9df259d3b497d342319b52bdeb0091ead86a3e0
Larry Li <larry.zy.li@gmail.com>
2018-02-15 09:29:24 -0600

    add nav props

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -6,45 +6,42 @@ import dropdownIcon from "../../icons/ic_arrow_drop_down_white_24px.svg";
 
 // COMPONENT
 class Navigation extends Component {
-    render() {
-        // DISPLAY RIGHT ICONS
-        const rightIcons = this.props.rightIcons.map((icon, index) => {
-            return (
-                <img
-                    key={index}
-                    className="nav-left-icon"
-                    src={icon}
-                    alt="control icon"
-                />
-            );
-        });
-        return (
-            <div className="nav-background">
-                <div className="nav-left">
-                    <img
-                        className="nav-left-icon"
-                        src={this.props.leftIcon}
-                        alt="menu icon"
-                        onClick={e => this.props.openMenu()}
-                    />
-                    <div className="nav-page-container">
-                        <div className="nav-title">
-                            <span className="nav-page-name">
-                                {this.props.title}
-                            </span>
-                            <span className="nav-page-subtitle">
-                                {this.props.filterName}
-                            </span>
-                        </div>
-                        {this.props.dropdown ? (
-                            <img src={dropdownIcon} alt="dropdown icon" />
-                        ) : null}
-                    </div>
-                </div>
-                <div className="nav-right">{rightIcons}</div>
+  render() {
+    // DISPLAY RIGHT ICONS
+    const rightIcons = this.props.rightIcons.map((icon, index) => {
+      return (
+        <img
+          key={index}
+          className="nav-left-icon"
+          src={icon}
+          alt="control icon"
+          onClick={this.props.rightActions[index]}
+        />
+      );
+    });
+    return (
+      <div className="nav-background">
+        <div className="nav-left">
+          <img
+            className="nav-left-icon"
+            src={this.props.leftIcon}
+            alt="menu icon"
+            onClick={e => this.props.leftAction()}
+          />
+          <div className="nav-page-container">
+            <div className="nav-title">
+              <span className="nav-page-name">{this.props.title}</span>
+              <span className="nav-page-subtitle">{this.props.filterName}</span>
             </div>
-        );
-    }
+            {this.props.dropdown ? (
+              <img src={dropdownIcon} alt="dropdown icon" />
+            ) : null}
+          </div>
+        </div>
+        <div className="nav-right">{rightIcons}</div>
+      </div>
+    );
+  }
 }
 
 // EXPORT COMPONENT
--- src/ducks/subredditReducer.js
+++ src/ducks/subredditReducer.js
@@ -1,102 +1,96 @@
-import axios from 'axios';
+import axios from "axios";
 
 const PULL_HOT = "PULL_HOT";
-const PULL_BEST = "PULL_BEST"
-
+const PULL_BEST = "PULL_BEST";
 
 //Axios call to pull "hot" posts from Reddit
 export function pullHot(after, limit) {
-    let url=`/api/hot?`
-    
-    if(after){
-        url += `after=${after}&`
-    }
-    if(limit){
-        url += `limit=${limit}&`
-    }
+  let url = `/api/hot?`;
 
-    return {
-        type: PULL_HOT,
-        payload: axios
-            .get(url)
-            .then(response => response.data)
-            .catch(console.log)
-    };
+  if (after) {
+    url += `after=${after}&`;
+  }
+  if (limit) {
+    url += `limit=${limit}&`;
+  }
+
+  return {
+    type: PULL_HOT,
+    payload: axios
+      .get(url)
+      .then(response => response.data)
+      .catch(console.log)
+  };
 }
 
 //Axios call to pull "best" posts from Reddit
-export function pullBest(after, limit){
-    let url=`/api/best?`
-    
-    if(after){
-        url += `after=${after}&`
-    }
-    if(limit){
-        url += `limit=${limit}&`
-    }
-    return {
-        type:PULL_BEST,
-        payload: axios
-            .get(url)
-            .then(response => response.data)
-            .catch(console.log)
-    }
+export function pullBest(after, limit) {
+  let url = `/api/best?`;
+
+  if (after) {
+    url += `after=${after}&`;
+  }
+  if (limit) {
+    url += `limit=${limit}&`;
+  }
+  return {
+    type: PULL_BEST,
+    payload: axios
+      .get(url)
+      .then(response => response.data)
+      .catch(console.log)
+  };
 }
 
-
 const initialState = {
-    posts: [],
-    after: '',
-    isLoading: false,
-    didError: false,
+  posts: [],
+  after: "",
+  isLoading: false,
+  didError: false
 };
 
 export default function userReducer(state = initialState, action) {
-    switch (action.type) {
+  switch (action.type) {
+    //PULL_HOT axios call is pending
+    case `${PULL_HOT}_PENDING`:
+      return Object.assign({}, state, { isLoading: true });
 
-        //PULL_HOT axios call is pending
-        case `${PULL_HOT}_PENDING`:
-            return Object.assign({}, state, { isLoading: true });
+    //Adding PULL_HOT response to the posts array on state
+    case `${PULL_HOT}_FULFILLED`:
+      return Object.assign({}, state, {
+        isLoading: false,
+        posts: action.payload.posts,
+        after: action.payload.after
+      });
 
-        //Adding PULL_HOT response to the posts array on state
-        case `${PULL_HOT}_FULFILLED`:
-            console.log(action.payload)
-            return Object.assign({}, state, {
-                isLoading: false,
-                posts: action.payload.posts,
-                after: action.payload.after
-            });
-        
-        //PULL_HOT axios call failed
-        case `${PULL_HOT}_REJECTED`:
-            return Object.assign({}, state, {
-                isLoading: false,
-                didError: true
-            });
+    //PULL_HOT axios call failed
+    case `${PULL_HOT}_REJECTED`:
+      return Object.assign({}, state, {
+        isLoading: false,
+        didError: true
+      });
 
-            
-        //PULL_BEST axios call is pending
-        case `${PULL_BEST}_PENDING`:
-            return Object.assign({}, state, { isLoading: true });
+    //PULL_BEST axios call is pending
+    case `${PULL_BEST}_PENDING`:
+      return Object.assign({}, state, { isLoading: true });
 
-        //Adding PULL_BEST response to the posts array on state
-        case `${PULL_BEST}_FULFILLED`:
-            console.log(action.payload)
-            return Object.assign({}, state, {
-                isLoading: false,
-                posts: action.payload.posts,
-                after: action.payload.after
-            });
-        
-        //PULL_BEST axios call failed
-        case `${PULL_BEST}_REJECTED`:
-            return Object.assign({}, state, {
-                isLoading: false,
-                didError: true
-            });
+    //Adding PULL_BEST response to the posts array on state
+    case `${PULL_BEST}_FULFILLED`:
+      console.log(action.payload);
+      return Object.assign({}, state, {
+        isLoading: false,
+        posts: action.payload.posts,
+        after: action.payload.after
+      });
 
+    //PULL_BEST axios call failed
+    case `${PULL_BEST}_REJECTED`:
+      return Object.assign({}, state, {
+        isLoading: false,
+        didError: true
+      });
 
-
-        default: return state;
-    }
+    default:
+      return state;
+  }
 }
