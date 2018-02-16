57dd7a26587c54d144f7588901bc94b0dd70e365
Larry Li <larry.zy.li@gmail.com>
2018-02-15 18:08:12 -0600

    add postcard controls, style postdata

--- src/components/Post/PostData.js
+++ src/components/Post/PostData.js
@@ -8,16 +8,30 @@ const PostData = ({ postData }) => {
     <div className="postdata-container">
       <div>
         {postData.image ? (
-          <img
-            className="postdata-image"
-            src={postData.image}
-            alt="link preview"
-          />
+          <a className="postdata-link" href={postData.url}>
+            <img
+              className="postdata-image"
+              src={postData.image}
+              alt="link preview"
+            />
+
+            <div className="postdata-link-info-container">
+              <div className="postdata-link-info">{postData.domain}</div>
+              <div className="postdata-link-info">{postData.url}</div>
+            </div>
+          </a>
         ) : null}
       </div>
       <div className="postdata-info-container">
         <div className="postdata-title">{postData.post_title}</div>
         <div className="postdata-data">
+          <span>
+            {postData.score > 10000
+              ? (postData.score / 1000).toFixed(1) + "k"
+              : postData.score}{" "}
+            points
+          </span>
+          {" • "}
           <span>{postData.author}</span>
           {" • "}
           <span>{moment(postData.created_at * 1000).fromNow()}</span>
