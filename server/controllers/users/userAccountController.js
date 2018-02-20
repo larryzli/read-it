e1ac750b4d978a36c3eb1336690a021ef92f0831
Larry Li <larry.zy.li@gmail.com>
2018-02-20 11:08:11 -0600

    get all subreddits working

--- server/controllers/users/userAccountController.js
+++ server/controllers/users/userAccountController.js
@@ -1,5 +1,5 @@
 const axios = require("axios");
-const querystring = require("querystring")
+const querystring = require("querystring");
 
 const getUserInfo = (req, res, next) => {
   if (req.user) {
@@ -26,52 +26,45 @@ const friend = (req, res, next) => {
 
   axios
     .put(
-    `https://oauth.reddit.com/api/v1/me/friends/${username}`,
-    {
-      api_type: "json",
-      name: `${username}`,
-      type: "friend",
-      container: ""
-    },
-    {
-      headers: {
-        Authorization: `Bearer ${req.user.accessToken}`,
-        "User-Agent": `web-app:navit:v0.0.1 (by /${userAgent})`
+      `https://oauth.reddit.com/api/v1/me/friends/${username}`,
+      {
+        api_type: "json",
+        name: `${username}`,
+        type: "friend",
+        container: ""
+      },
+      {
+        headers: {
+          Authorization: `Bearer ${req.user.accessToken}`,
+          "User-Agent": `web-app:navit:v0.0.1 (by /${userAgent})`
+        }
       }
-    }
     )
     .then(response => res.status(200).json(response.data))
     .catch(console.log);
-
 };
 
-// NOT WORKING
 const unfriend = (req, res, next) => {
   const { username } = req.params;
   const userAgent = req.user._json.subreddit.display_name_prefixed;
 
   axios
-    .delete(
-    `https://oauth.reddit.com/api/v1/me/friends/${username}`,
-    {
+    .delete(`https://oauth.reddit.com/api/v1/me/friends/${username}`, {
       headers: {
         Authorization: `Bearer ${req.user.accessToken}`,
         "User-Agent": `web-app:navit:v0.0.1 (by /${userAgent})`
       }
-    }
-    )
+    })
     .then(response => res.status(200).json(response.data))
     .catch(console.log);
-
 };
 
 const getUserAbout = (req, res, next) => {
-  const { username, sort, t } = req.query
-  let baseURL = `https://oauth.reddit.com/user/${username}/overview?sort=top`
+  const { username, sort, t } = req.query;
+  let baseURL = `https://oauth.reddit.com/user/${username}/overview?sort=top`;
   if (sort) {
-    baseURL = `https://oauth.reddit.com/user/${username}/overview?sort=${sort}&t=${t}`
+    baseURL = `https://oauth.reddit.com/user/${username}/overview?sort=${sort}&t=${t}`;
   }
-
 
   axios
     .get(baseURL, {
@@ -88,25 +81,25 @@ const getUserAbout = (req, res, next) => {
 // SR_NAME IS A STRING OF THE NAME OF THE SUBREDDIT
 // ACTION IS A STRING THAT NEEDS TO BE "SUB" OR "UNSUB"
 const subscribe = (req, res, next) => {
-  const { sr_name, action } = req.body
+  const { sr_name, action } = req.body;
 
   axios
-    .post(`https://oauth.reddit.com/api/subscribe`,
-    querystring.stringify({
-      api_type: "json",
-      action: action,
-      sr_name: sr_name
-    }),
-    {
-      headers: {
-        Authorization: `bearer ${req.user.accessToken}`
+    .post(
+      `https://oauth.reddit.com/api/subscribe`,
+      querystring.stringify({
+        api_type: "json",
+        action: action,
+        sr_name: sr_name
+      }),
+      {
+        headers: {
+          Authorization: `bearer ${req.user.accessToken}`
+        }
       }
-    })
+    )
     .then(response => res.status(200).json(response.data))
     .catch(console.log);
 };
-
-
 
 module.exports = {
   getUserInfo,
@@ -114,5 +107,5 @@ module.exports = {
   getUserAbout,
   friend,
   unfriend,
-  subscribe,
+  subscribe
 };
