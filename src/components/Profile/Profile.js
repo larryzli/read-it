52141e1c14cb0519509340d3c9803b13f280babd
Jordan Snow <snowstorm1989@gmail.com>
2018-02-22 16:43:08 -0600

    Finish Profile Component

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import { connect } from "react-redux"
 import axios from 'axios'
+import moment from "moment";
 
 import loading from '../../icons/loading/loading-cylon-red.svg'
 
 //IMPORT COMPONENTS
 import PostNavigation from '../../components/Navigation/PostNavigation'
+import PostCard from "../PostCard/PostCard";
 
 class Profile extends Component {
     constructor(props) {
@@ -55,11 +57,13 @@ class Profile extends Component {
     };
     render(){
         console.log(this.state)
+        // LOADER
         const loader = (
             <div className="loader-wrapper" key={"loader"}>
                 <img src={loading} className="loader-svg" alt="loading" />
             </div>
         );
+        // DRAWER
         const sortDrawer = (
             <div className="drawer-wrapper" onClick={e => this.toggleSort()}>
               <div className="drawer-container">
@@ -78,6 +82,47 @@ class Profile extends Component {
               </div>
             </div>
           );
+          //PROFILE COMMENTS AND POSTS
+           const posts = [];
+            this.state.posts.forEach((post, index) => {
+                posts.push(
+                    <PostCard
+                      key={index}
+                      title={post.data.title ? post.data.title : post.data.link_title }
+                      domain={post.data.domain ? post.data.domain : post.data.subreddit }
+                      subreddit={post.data.subreddit}
+                      author={post.data.author}
+                      thumbnail={post.data.thumbnail ? post.data.thumbnail : null}
+                      comments={post.data.num_comments}
+                      score={post.data.score}
+                      subredditID={post.data.subreddit_id}
+                      created={moment(post.data.created_utc * 1000).fromNow()}
+                      url={post.data.url}
+                      over18={post.data.over_18}
+                      postID={post.data.id}
+                      likes={post.data.likes}
+                      saved={post.data.saved}
+                      enableControls={this.props.enableControls}
+                      hidden={post.data.hidden}
+                      clicked={post.data.clicked}
+                      visited={post.data.visited}
+                      pinned={post.data.pinned}
+                      archived={post.data.archived}
+                      spoiler={post.data.spoiler}
+                      locked={post.data.locked}
+                      stickied={post.data.stickied}
+                      edited={post.data.edited}
+                      gilded={post.data.gilded}
+                      isRedditMedia={post.data.is_reddit_media}
+                      showSubredditControl={this.props.showSubredditControl}
+                      type={post.kind}
+                      link_id={post.data.link_id || ""}
+                    />
+                )
+                }
+                
+        )
+    
         return (
             <div className="profile-page">
                 {this.state.showSortDrawer ? sortDrawer : null}
@@ -89,7 +134,18 @@ class Profile extends Component {
 
                 />
                 {this.state.loading ? loader : null}
-                    PROFILE PAGE
+                <div>
+                    {this.state.username}
+                </div>
+                <div>
+                    Link Karma: {this.state.linkKarma}
+                    <span/>
+                    Comment Karma: {this.state.commentKarma}
+                </div>
+                <div className="posts">
+                  {posts}
+                </div>
+                
                 
             </div>
 
